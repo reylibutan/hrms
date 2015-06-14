79a0391f7e226df69e7b85e23d17e4bfc4208b03
reylibutan <reylibutan@gmail.com>
2015-06-14 17:42:24 +0800

    Setup for Logging (Logback)

--- src/main/java/com/reylibutan/tabularasa/controller/HiController.java
+++ src/main/java/com/reylibutan/tabularasa/controller/HiController.java
@@ -1,5 +1,7 @@
 package com.reylibutan.tabularasa.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/hi")
 public class HiController {
 	
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@RequestMapping("/hi")
 	public String hi(@RequestParam(value="name", required=false, defaultValue="Chuck Norris") String name, Model model) {
+		log.info("Saying hi to {}.", name);
+		
 		model.addAttribute("name", name);
+		
 		return "hi";
 	}
 }
