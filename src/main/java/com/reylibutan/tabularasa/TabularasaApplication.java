f12f705fd00c0c5166efe84da108714d1346f564
Rey Libutan <reylibutan@gmail.com>
2015-06-19 09:05:39 +0800

    Added Internationalization (basic setup)

--- src/main/java/com/reylibutan/tabularasa/TabularasaApplication.java
+++ src/main/java/com/reylibutan/tabularasa/TabularasaApplication.java
@@ -1,6 +1,7 @@
 package com.reylibutan.tabularasa;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,21 +9,20 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import com.reylibutan.tabularasa.interceptor.SampleInterceptor;
 
 @SpringBootApplication
-/* These are the added annotations for @SpringBootApplication
- * @Configuration
- * @EnableAutoConfiguration
- * @EnableWebMvc (added if it sees spring-webmvc on the classpath)
- * @ComponentScan
-*/
 public class TabularasaApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    private final String PARAM_LANGUAGE = "lang";
+	
+	public static void main(String[] args) {
     	SpringApplication.run(TabularasaApplication.class, args);
     }
     
@@ -42,11 +42,26 @@ public class TabularasaApplication extends SpringBootServletInitializer {
     }
     
     @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName(this.PARAM_LANGUAGE);
+        return lci;
+    }
+    
+    @Bean
     public WebMvcConfigurerAdapter adapter() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addInterceptors(InterceptorRegistry registry) {
             	registry.addInterceptor(new SampleInterceptor()).addPathPatterns("/sample/*");
+            	registry.addInterceptor(localeChangeInterceptor());
             }
         };
     }
