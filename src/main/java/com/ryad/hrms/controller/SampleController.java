3bfa3e3b07b81cd7fd84e1df535e1e436f1d3876
Rey Libutan <reylibutan@gmail.com>
2015-07-07 12:42:59 +0800

    Fixed missing @RequestMapping

--- src/main/java/com/ryad/hrms/controller/SampleController.java
+++ src/main/java/com/ryad/hrms/controller/SampleController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SampleController {
 	
+	@RequestMapping("/")
+	public String index() {
+		return this.login();
+	}
+	
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
@@ -14,10 +19,5 @@ public class SampleController {
 	@RequestMapping("/home")
 	public String home() {
 		return "home";
-	}
-	
-	@RequestMapping
-	public String index() {
-		return this.home();
 	}
 }
