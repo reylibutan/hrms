78b43475a25e201547f70eb7ab3ebe198f09e904
reylibutan <reylibutan@gmail.com>
2015-07-08 23:57:02 +0800

    Updated Patient list sample data table

--- src/main/java/com/ryad/hrms/controller/SampleController.java
+++ src/main/java/com/ryad/hrms/controller/SampleController.java
@@ -1,7 +1,16 @@
 package com.ryad.hrms.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ryad.hrms.dto.PatientDTO;
 
 @Controller
 public class SampleController {
@@ -17,7 +26,27 @@ public class SampleController {
 	}
 	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		int max = 125;
+		List<PatientDTO> patients = new ArrayList<PatientDTO>(max);
+		
+		LocalDate localDate = new LocalDate();
+		DateTimeFormatter dateDisplayFormat = DateTimeFormat.forPattern("MMM dd, yyyy");
+		
+		PatientDTO patient;
+		for(int i = 0 ; i < max; i++) {
+			patient = new PatientDTO();
+			patient.setFullName("Juan de la Cruz #" + i);
+			patient.setCodeName("JUAN#" + i);
+			patient.setUniqueIdCode("AA-BB-" + String.format("%02d", (i % 100)));
+			patient.setBirthdate(dateDisplayFormat.print(localDate.plusDays(i)));
+			patient.setSex(i % 3 < 2 ? "Male" : "Female");
+			
+			patients.add(patient);
+		}
+		
+		model.addAttribute("patients", patients);
+		
 		return "home";
 	}
 }
