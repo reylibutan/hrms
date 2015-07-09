200f5ce20afc5bebfa4968e52b119d75ed65eed7
Rey Libutan <reylibutan@gmail.com>
2015-07-09 17:48:10 +0800

    Added Create new record

--- src/main/resources/static/theme/material-admin/assets/js/core/demo/DemoTableDynamic.js
+++ src/main/resources/static/theme/material-admin/assets/js/core/demo/DemoTableDynamic.js
@@ -30,21 +30,19 @@
 		}
 
 		// Init the demo DataTables
-		this._createDataTable1();
-		this._createDataTable2();
+		this._createPatientsDataTable();
 	};
 
-	p._createDataTable1 = function() {
-		$('#datatable1').DataTable({
-			"dom": 'lCfrtip',
+	p._createPatientsDataTable = function() {
+		$('#patientsDataTable').DataTable({
+			searching: false,
 			"order": [],
-			"colVis": {
-				"buttonText": "Columns",
-				"overlayFade": 0,
-				"align": "right"
-			},
+			"aoColumnDefs": [{
+	            'bSortable': false,
+	            'aTargets': [5]
+	        }],
 			"language": {
-				"lengthMenu": '_MENU_ entries per page',
+				"lengthMenu": '_MENU_ records per page',
 				"search": '<i class="fa fa-search"></i>',
 				"paginate": {
 					"previous": '<i class="fa fa-angle-left"></i>',
@@ -52,81 +50,6 @@
 				}
 			}
 		});
-
-		$('#datatable1 tbody').on('click', 'tr', function() {
-			$(this).toggleClass('selected');
-		});
-	};
-
-	p._createDataTable2 = function() {
-		var table = $('#datatable2').DataTable({
-			"dom": 'T<"clear">lfrtip',
-			"ajax": $('#datatable2').data('source'),
-			"columns": [
-				{
-					"class": 'details-control',
-					"orderable": false,
-					"data": null,
-					"defaultContent": ''
-				},
-				{"data": "name"},
-				{"data": "position"},
-				{"data": "office"},
-				{"data": "salary"}
-			],
-			"tableTools": {
-				"sSwfPath": $('#datatable2').data('swftools')
-			},
-			"order": [[1, 'asc']],
-			"language": {
-				"lengthMenu": '_MENU_ entries per page',
-				"search": '<i class="fa fa-search"></i>',
-				"paginate": {
-					"previous": '<i class="fa fa-angle-left"></i>',
-					"next": '<i class="fa fa-angle-right"></i>'
-				}
-			}
-		});
-		
-		//Add event listener for opening and closing details
-		var o = this;
-		$('#datatable2 tbody').on('click', 'td.details-control', function() {
-			var tr = $(this).closest('tr');
-			var row = table.row(tr);
-
-			if (row.child.isShown()) {
-				// This row is already open - close it
-				row.child.hide();
-				tr.removeClass('shown');
-			}
-			else {
-				// Open this row
-				row.child(o._formatDetails(row.data())).show();
-				tr.addClass('shown');
-			}
-		});
-	};
-
-	// =========================================================================
-	// DETAILS
-	// =========================================================================
-
-	p._formatDetails = function(d) {
-		// `d` is the original data object for the row
-		return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
-				'<tr>' +
-				'<td>Full name:</td>' +
-				'<td>' + d.name + '</td>' +
-				'</tr>' +
-				'<tr>' +
-				'<td>Extension number:</td>' +
-				'<td>' + d.extn + '</td>' +
-				'</tr>' +
-				'<tr>' +
-				'<td>Extra info:</td>' +
-				'<td>And any further details here (images etc)...</td>' +
-				'</tr>' +
-				'</table>';
 	};
 
 	// =========================================================================
