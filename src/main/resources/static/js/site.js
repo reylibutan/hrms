f8e0fea846957208c5cf360cbc53b29d42771758
reylibutan <reylibutan@gmail.com>
2015-07-20 02:23:32 +0800

    Added error message display for Login page
    - to be improved, see login.html @TODO

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -3,7 +3,15 @@ $(document).ready(function() {
 });
 
 function initButtonEvents() {
+	// submit logout form
 	$("#logoutBtn").click(function() {
 		$("#logoutForm").submit();
+	});
+	
+	// custom animation on alert close
+	$(".alert .close").click(function() {
+		$(this).closest(".alert").slideUp(150, function() {
+			$(this).remove();
+		});
 	});
 }
